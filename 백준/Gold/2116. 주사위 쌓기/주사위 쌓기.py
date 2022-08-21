c957fb29dd7fea2854a30079680723c354d6e398
bshello <110216632+bshello@users.noreply.github.com>
2022-08-21 19:14:18 +0900

    [Gold IV] Title: 주사위 쌓기, Time: 592 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
+++ 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
@@ -4,12 +4,9 @@ for _ in range(t):
     n = list(map(int, input().split()))
     dic = [n[0],n[1],n[2],n[4],n[3],n[5]]
     dice.append(dic)
-
 i = 0
 cnt = 0
 ans = 0
-tt = list()
-ttt = list()
 while i<6:
 
     temp = 0
@@ -17,7 +14,6 @@ while i<6:
     templ.append(dice[0][i])
     templ.append(dice[0][5-i])
 
-    ttt.append(templ)
     if 6 not in templ:
         temp += 6
     elif 5 not in templ:
@@ -33,14 +29,13 @@ while i<6:
         templ = list()
         templ.append(dice[j+1][i])
         templ.append(dice[j+1][5 - i])
-        ttt.append(templ)
+
         if 6 not in templ:
             temp += 6
         elif 5 not in templ:
             temp += 5
         else:
             temp += 4
-    tt.append(temp)
     cnt += 1
     i = cnt
     if ans < temp:
