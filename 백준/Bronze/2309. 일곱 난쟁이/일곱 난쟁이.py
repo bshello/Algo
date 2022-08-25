b266b2ca03104f125aa0bcd8117b61859a19d69d
bshello <110216632+bshello@users.noreply.github.com>
2022-08-25 17:10:54 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,17 +1,16 @@
-dwarf = list()
-m = 9
-for _ in range(m):
-    n = int(input())
-    dwarf.append(n)
-result = list()
-for i in range(1<<m):
-    temp = []
-    for j in range(m):
-        if i & (1<<j):
+n = 9
+d = [input() for _ in range(n)]
+dwarf = list(map(int, d))
+res = list()
+for i in range(1 << n):
+    temp = list()
+    for j in range(n):
+        if i & (1 << j):
             temp.append(dwarf[j])
-        if len(temp) == 7:
-            if sum(temp) == 100:
-                temp.sort()
-                result.append(temp)
-for k in range(7):
-    print(result[0][k])
+    if len(temp) == 7:
+        if sum(temp) == 100:
+            res.append(temp)
+
+res[0].sort()
+for i in res[0]:
+    print(i)
