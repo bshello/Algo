fb64f6ec80c81434b9fc2ebffec7c80c3f061913
bshello <110216632+bshello@users.noreply.github.com>
2022-09-20 21:09:24 +0900

    [Silver V] Title: 소수 찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,18 +1,12 @@
-N = int(input())
+n = int(input())
 num = list(map(int, input().split()))
-ans = 0
-for i in num:
-    for j in range(N):
-        cnt = 0
-        if num[j]>2:
-            for k in range(1,num[j]):
-                if num[j] % k != 0:
-                    cnt+=1
-                    if cnt == num[j]-2:
-                        ans+=1
+ans = n
+if 1 in num:
+    ans -= 1
+for i in range(n):
+    for j in range(2, int(num[i]**(1/2))+1):
+        if num[i] % j == 0:
+            ans -= 1
+            break
 
-answer = int(ans/N)
-if 2 in num:
-    answer+=1
-
-print(answer)
+print(ans)
