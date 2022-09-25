915d4f36e7b4637a08d2ffa9ba8a7e963a70dd21
bshello <110216632+bshello@users.noreply.github.com>
2022-09-26 00:21:58 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,13 +1,9 @@
 n = int(input())
-temp = 1
-for i in range(1, n+1):
-    temp *= i
-ans = list(str(temp))
-ans.reverse()
-answer = list()
 cnt = 0
-for j in range(len(ans)):
-    cnt +=1
-    if ans[j] != '0':
-        break
-print(cnt-1)
+for i in range(5, n+1, 5):
+    if i % 5 == 0:
+        while i % 5 == 0:
+            i //= 5
+            cnt += 1
+
+print(cnt)
