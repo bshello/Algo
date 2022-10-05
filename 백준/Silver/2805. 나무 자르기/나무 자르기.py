d1895e1fb67035c237b2d699fc9f30527a80202a
bshello <110216632+bshello@users.noreply.github.com>
2022-10-05 18:58:15 +0900

    [Silver II] Title: 나무 자르기, Time: 4936 ms, Memory: 148408 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+s = 1
+e = arr[-1]
+ans = 0
+while s <= e:
+    tmp = 0
+    mid = (s + e) // 2
+
+    for i in arr:
+        if i > mid:
+            tmp += i - mid
+
+    if tmp >= m:
+        ans = max(ans, mid)
+        s = mid + 1
+    elif tmp < m:
+        e = mid - 1
+    else:
+        ans = max(ans, mid)
+        break
+
+print(ans)
