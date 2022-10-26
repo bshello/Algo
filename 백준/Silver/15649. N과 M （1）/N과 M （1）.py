48fc52babb75a5a99812738dcc7f49541ecde8e1
bshello <110216632+bshello@users.noreply.github.com>
2022-10-26 20:16:26 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 119152 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,18 @@
-def dfs(N, M):
-    global num
-
-    if len(num) == M:
-        print(*num)
+def solve(depth):
+    if depth == n:
+        print(*arr)
         return
-
-    for i in range(1, N + 1):
-        if i not in num:
-            num.append(i)
-            dfs(N, M)
-            num.pop()
-
-N, M = map(int, input().split())
-num = []
-dfs(N, M)
+    for i in range(m):
+        if vis[i] == 1:
+            continue
+        arr[depth] = i + 1
+        vis[i] = 1
+        solve(depth + 1)
+        vis[i] = 0
 
 
+m, n = map(int, input().split())
+arr = [0] * n
+vis = [0] * m
 
+solve(0)
