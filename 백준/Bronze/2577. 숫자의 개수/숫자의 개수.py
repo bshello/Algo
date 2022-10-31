1cb347ac641a03426499b64dd8998fdd45c78ed2
bshello <110216632+bshello@users.noreply.github.com>
2022-10-31 13:07:26 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,13 +1,13 @@
-inp = list()
-for _ in range(3):
-    inp.append(int(input()))
-s = 1
-for i in inp:
-    s *= i
-num = list(str(s))
-ans = {'0':0, '1':0, '2':0, '3':0, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0}
-for j in num:
-    if j in ans:
-        ans[j] += 1
-for k in ans.values():
-    print(k)
+A = int(input())
+B = int(input())
+C = int(input())
+
+num = A*B*C
+num = str(num)
+res = [0]*10
+for n in num:
+    res[int(n)] += 1
+
+for i in range(10):
+    print(res[i])
+print()
