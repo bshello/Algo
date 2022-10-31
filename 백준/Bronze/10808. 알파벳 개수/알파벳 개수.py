5eada85aaff0a089df433d4767f6c874d2d90a49
bshello <110216632+bshello@users.noreply.github.com>
2022-10-31 12:57:19 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -1,12 +1,12 @@
-alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-place = list()
-for _ in range(26):
-  place.append(0)
+word = input()
 
-sen = input()
-temp = dict()
-for i in sen:
-  for j in range(len(alpha)):
-    if i == alpha[j]:
-      place[j] += 1
-print(*place)
+res = [0]*26
+
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+for i in range(26):
+    for j in range(len(word)):
+        if word[j] == alpha[i]:
+            res[i] += 1
+
+print(*res)
