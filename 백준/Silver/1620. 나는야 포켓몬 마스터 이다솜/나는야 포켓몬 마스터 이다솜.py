b962915805e4ebec5e173efdd260ac6952cf5e50
bshello <110216632+bshello@users.noreply.github.com>
2023-06-03 17:04:43 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 324 ms, Memory: 136208 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,16 +1,16 @@
 import sys
-
+input = sys.stdin.readline
 N, M = map(int, input().split())
 
 pocketList = []
 pocketDict = dict()
 for i in range(N):
-    pocketmon = input()
+    pocketmon = input().rstrip()
     pocketList.append(pocketmon)
     pocketDict[pocketmon] = i
 
 for _ in range(M):
-    problem = input()
+    problem = input().rstrip()
     if problem.isalpha():
         print(pocketDict.get(problem) + 1)
     else:
