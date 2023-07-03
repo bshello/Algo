c85d9e6f7a23fb01da00cf787358d8446768216c
bshello <110216632+bshello@users.noreply.github.com>
2023-07-03 17:29:52 +0900

    [Silver I] Title: 절댓값 힙, Time: 260 ms, Memory: 121716 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -13,19 +13,6 @@ for i in range(N):
         if min_heap:
             min_absValue, min_realValue = heapq.heappop(min_heap)
             ans = min_realValue
-            tmp = []
-            while min_heap and min_heap[0][0] == min_absValue:
-                tmp_absValue, tmp_realValue = heapq.heappop(min_heap)
-                if tmp_realValue < min_realValue:
-                    ans = tmp_realValue
-                    heapq.heappush(tmp, (min_absValue, min_realValue))
-                else:
-                    heapq.heappush(tmp, (tmp_absValue, tmp_realValue))
-
-            while tmp:
-                tmp_absValue, tmp_realValue = heapq.heappop(tmp)
-                heapq.heappush(min_heap, (tmp_absValue, tmp_realValue))
-
             print(ans)
         else:
             print(0)
