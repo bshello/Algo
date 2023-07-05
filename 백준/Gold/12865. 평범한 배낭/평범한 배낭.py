347ca077a5110c107597ab1efdc3e5975b0b1038
bshello <110216632+bshello@users.noreply.github.com>
2023-07-05 22:44:58 +0900

    [Gold V] Title: 평범한 배낭, Time: 348 ms, Memory: 194188 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,21 +1,21 @@
 import sys
+
 input = sys.stdin.readline
-N, M = map(int, input().split())
-Item = [(0, 0)]
-W = [0] * (M + 1)
-chart = [[0] * (M + 1) for _ in range(N + 1)]
+
+N, K = map(int, input().split())
+bag = [[0] * (K + 1) for _ in range(N + 1)]
+stuff = [(0, 0)]
 for _ in range(N):
-    w, v = map(int, input().split())
-    Item.append((w, v))
+    W, V = map(int, input().split())
+    stuff.append((W, V))
 
 for i in range(1, N + 1):
-    for j in range(1, M + 1):
-        w = Item[i][0]
-        v = Item[i][1]
+    for j in range(1, K + 1):
+        weight = stuff[i][0]
+        value = stuff[i][1]
 
-        if j < w:
-            chart[i][j] = chart[i - 1][j]
+        if j < weight:
+            bag[i][j] = bag[i - 1][j]
         else:
-            chart[i][j] = max(chart[i - 1][j], v + chart[i - 1][j - w])
-
-print(chart[-1][-1])
+            bag[i][j] = max(bag[i - 1][j - weight] + value, bag[i - 1][j])
+print(bag[N][K])
